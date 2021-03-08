8e4ab6698c4fa353bbe8c923b2d722337e3f2d5f
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-08 09:20:54 -0300

    add simple css

--- reading-list/src/App.js
+++ reading-list/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Booklist from './components/Booklist'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Booklist />
     </div>
   )
 }
