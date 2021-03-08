382b5983959fc68f1bddf8f363d0081fcfb5011e
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-08 10:30:09 -0300

    add Theme context

--- reading-list/src/components/Navbar.js
+++ reading-list/src/components/Navbar.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 class Navbar extends Component {
+    // Acessa o contexto de tema da aplicação. 
+    static contextType = ThemeContext
     render() { 
+        // Retorna o componente. 
         return ( 
-            <nav>
-                <h1> Context app </h1>
-                <ul>
-                    <li> Home </li>
-                    <li> About </li>
-                    <li> Contact </li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                // Desestrutura as propriedades do contexto.
+                const { isLightTheme, light, dark } = context
+                // Define qual tema estara ativo com base no estado de isLightTheme.
+                const theme = isLightTheme ? light : dark
+
+                return(
+                    // Define as propriedades dentro do estilo dinamicamente para ficar de acordo com o tema.
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                        <h1> Context app </h1>
+                        <ul>
+                            <li> Home </li>
+                            <li> About </li>
+                            <li> Contact </li>
+                        </ul>
+                    </nav>
+                )
+            }}
+            </ThemeContext.Consumer>
          )
     }
 }
